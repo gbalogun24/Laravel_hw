44c5731ec2727271fdc77503b18b2190a3353926
gafar <gb262@njit.edu>
2018-10-13 20:49:52 -0400

    Feature: Set up a link on Home page to About me page with head, header,and footer files

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+@section('content')
+<html lang="en">
+
+<h1>Hello. Welcome to About Page</h1>
+
+<p>This is my about page</p>
+
+
+@stop
+
+
+
+
+
+
+
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+@section('content')
+<html lang="en">
+
+<h1>Hello. Welcome to My Homepage</h1>
+
+<a href="/about">About Me</a>
+
+
+@stop
+
+
+
+
+
+
+
+</html>
